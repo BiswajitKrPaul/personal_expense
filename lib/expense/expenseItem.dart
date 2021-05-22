942d1b195934e9960ab83b24629fd12852b8e457
Biswajit Paul <biswajitkmrpl46@gmail.com>
2021-05-22 09:59:07 +0530

    converted ExpenseItem widget to Stateful widget

--- lib/expense/expenseItem.dart
+++ lib/expense/expenseItem.dart
@@ -2,62 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../database/transaction.dart';
 
-class ExpenseItem extends StatelessWidget {
+class ExpenseItem extends StatefulWidget {
   final Transaction transaction;
 
   ExpenseItem({this.transaction});
 
   @override
+  _ExpenseItemState createState() => _ExpenseItemState();
+}
+
+class _ExpenseItemState extends State<ExpenseItem> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      name: 'iPhone 12 mini',
+      price: 69990,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      name: 'MacBook Air M1',
+      price: 92492,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5.0,
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.all(
-              15.0,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.indigo,
-                style: BorderStyle.solid,
-                width: 1,
-              ),
-            ),
-            padding: EdgeInsets.all(
-              8.0,
-            ),
-            child: Text(
-              '₹ ${transaction.price.toString()}',
-              style: TextStyle(
-                fontSize: 15.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.indigo,
-              ),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+      children: transactions.map((tx) {
+        return (Card(
+          elevation: 5.0,
+          child: Row(
             children: [
-              Text(
-                transaction.name,
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.normal,
+              Container(
+                margin: EdgeInsets.all(
+                  15.0,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.indigo,
+                    style: BorderStyle.solid,
+                    width: 1,
+                  ),
+                ),
+                padding: EdgeInsets.all(
+                  8.0,
+                ),
+                child: Text(
+                  '₹ ${tx.price.toString()}',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo,
+                  ),
                 ),
               ),
-              Text(
-                DateFormat(DateFormat.YEAR_ABBR_MONTH_DAY)
-                    .format(transaction.date),
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tx.name,
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  Text(
+                    DateFormat(DateFormat.YEAR_ABBR_MONTH_DAY).format(tx.date),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
               )
             ],
-          )
-        ],
-      ),
+          ),
+        ));
+      }).toList(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expense/database/transaction.dart';
 import 'package:personal_expense/expense/expenseItem.dart';
 
 void main() {
@@ -9,21 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final List<Transaction> transaction = [
-      Transaction(
-        id: 't1',
-        name: 'iPhone 12 mini',
-        price: 69990,
-        date: DateTime.now(),
-      ),
-      Transaction(
-        id: 't2',
-        name: 'MacBook Air M1',
-        price: 92492,
-        date: DateTime.now(),
-      ),
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -32,13 +16,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Container(
-          child: Column(
-            children: transaction.map((tx) {
-              return ExpenseItem(
-                transaction: tx,
-              );
-            }).toList(),
-          ),
+          child: ExpenseItem(),
         ),
       ),
     );
